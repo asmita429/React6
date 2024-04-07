c6e6be20ef7bd4052efe82166d50b796c6cda988
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-07 09:23:41 +0545

    form label added, with htmlFor for attribute

--- forms/src/Form.jsx
+++ forms/src/Form.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("asmita");
-  //   let [fullName, setFullName] = useState("");
+  let [fullName, setFullName] = useState("");
 
   let handleNameChange = (event) => {
-    // console.log(event.target.value);
     setFullName(event.target.value);
   };
   return (
     <form>
+      <label htmlFor="username">Full Name:</label>
       <input
         type="text"
         placeholder="enter name"
         value={fullName}
         onChange={handleNameChange}
+        id="username"
       />
       <button>submit</button>
     </form>
