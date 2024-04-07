7180d1d76ccbce652638778827502dba87562724
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-07 10:35:11 +0545

    added a password field

--- forms/src/Form.jsx
+++ forms/src/Form.jsx
@@ -4,21 +4,27 @@ export default function Form() {
   let [formData, setFormData] = useState({
     fullName: "",
     username: "",
+    password: "",
   });
 
   let handleFormInput = (event) => {
-    let fieldName = event.target.name;
-    let newValue = event.target.value;
+    // let fieldName = event.target.name;
+    // let newValue = event.target.value;
+
     setFormData((currData) => {
-      currData[fieldName] = newValue;
-      return { ...currData };
+      //   currData[fieldName] = newValue;
+      //   return { ...currData, [fieldName]: newValue };
+
+      return { ...currData, [event.target.name]: event.target.value };
     });
   };
   let handleOnSubmit = (event) => {
+    console.log(formData);
     event.preventDefault();
     setFormData({
       fullName: "",
       username: "",
+      password: "",
     });
   };
   return (
@@ -32,6 +38,8 @@ export default function Form() {
         id="fullName"
         name="fullName"
       />
+      <br />
+      <br />
       <label htmlFor="username">Username:</label>
       <input
         type="text"
@@ -40,6 +48,17 @@ export default function Form() {
         onChange={handleFormInput}
         id="username"
         name="username"
+      />
+      <br />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        placeholder="enter password"
+        value={formData.password}
+        onChange={handleFormInput}
+        id="password"
+        name="password"
       />
       <button>submit</button>
     </form>
